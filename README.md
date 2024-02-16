f341169959695e8c23f6e4c18981628b98d5db43
Barbara-Mae <barbara.lewis@valtech.com>
2024-02-16 10:59:44 -0600

    Initial draft

---
+++ README.md
@@ -0,0 +1,4 @@
+# Create-A-Brief Form
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/barbaramae/pen/VwRVNdZ](https://codepen.io/barbaramae/pen/VwRVNdZ).
+
---
+++ dist/script.js
@@ -0,0 +1,61 @@
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  //console.log(form.querySelector('input[name="q1"]'));
+  //const formData = new FormData(form);
+  //console.log("formData = " + formData.get("q1"));
+
+  const data = new FormData(event.target);
+  //console.log("data = " + data);
+  let formJSON = Object.fromEntries(data.entries());
+
+  //$("#answers").html(JSON.stringify(formJSON));
+
+  $("#resultsQ1 span").text(formJSON.q1);
+  $("#resultsQ2 span").text(formJSON.q2);
+  $("#resultsQ3 span").text(formJSON.q3);
+  $("#resultsQ4 span").text(formJSON.q4);
+  $("#resultsQ5 span").text(formJSON.q5);
+  $("#resultsQ6 span").text(formJSON.q6);
+  $("#resultsQ7 span").text(formJSON.q7);
+
+  if (formJSON.q8 === undefined) {
+    $("#results_Confirm1").show();
+  }
+
+  if (formJSON.q9 === undefined) {
+    $("#results_Confirm2").show();
+  }
+
+  $("#results").show();
+  $("#questions").hide();
+}
+
+const form = document.querySelector("#questions");
+form.addEventListener("submit", handleFormSubmit);
+
+$("#editBrief").click(function () {
+  $("#results").hide();
+  $("#questions").show();
+  document.getElementById("copyIcon").classList.remove("bi-check-lg");
+  document.getElementById("copyIcon").classList.add("bi-clipboard-plus-fill");
+});
+
+$("#copyBrief").click(function () {
+  let text = document.getElementById("details").innerText;
+  console.log(text);
+  //navigator.clipboard.writeText(text);
+  const copyContent = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log("Content copied to clipboard");
+      document
+        .getElementById("copyIcon")
+        .classList.remove("bi-clipboard-plus-fill");
+      document.getElementById("copyIcon").classList.add("bi-check-lg");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
+  copyContent();
+});
